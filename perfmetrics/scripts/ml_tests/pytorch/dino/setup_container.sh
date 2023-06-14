431df44d98e6957a397d828fe6de45c6650ba814
Jiaxun Song <songjiaxun@google.com>
2023-06-14 19:48:25 +0000

    switch back to the kernel-cache bypass

--- perfmetrics/scripts/ml_tests/pytorch/dino/setup_container.sh
+++ perfmetrics/scripts/ml_tests/pytorch/dino/setup_container.sh
@@ -26,12 +26,23 @@ nohup /pytorch_dino/gcsfuse/gcsfuse --foreground --type-cache-ttl=1728000s \
 
 # Update the pytorch library code to bypass the kernel-cache
 echo "Updating the pytorch library code to bypass the kernel-cache..."
-echo "        os.system(\"sh -c 'echo 3 > /proc/sys/vm/drop_caches'\")" > bypassed_code.py
+echo "
+def pil_loader(path: str) -> Image.Image:
+    fd = os.open(path, os.O_DIRECT)
+    f = os.fdopen(fd, \"rb\")
+    img = Image.open(f)
+    rgb_img = img.convert(\"RGB\")
+    f.close()
+    return rgb_img
+" > bypassed_code.py
 
-controller_file="dino/main_dino.py";
-x=$(grep -n "# ============ training one epoch of DINO ... ============" $controller_file | cut -f1 -d ':');
-x=$((x - 2));
-sed -i "$x"'r bypassed_code.py' $controller_file;
+folder_file="/opt/conda/lib/python3.8/site-packages/torchvision/datasets/folder.py"
+x=$(grep -n "def pil_loader(path: str) -> Image.Image:" $folder_file | cut -f1 -d ':')
+y=$(grep -n "def accimage_loader(path: str) -> Any:" $folder_file | cut -f1 -d ':')
+y=$((y - 2))
+lines="$x,$y"
+sed -i "$lines"'d' $folder_file
+sed -i "$x"'r bypassed_code.py' $folder_file
 
 # Fix the caching issue - comes when we run the model first time with 8
 # nproc_per_node - by downloading the model in single thread environment.

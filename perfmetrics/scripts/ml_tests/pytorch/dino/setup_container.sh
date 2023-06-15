e720bc1da822f285bd6f8550b2de19394fdfcaf2
Jiaxun Song <songjiaxun@google.com>
2023-06-15 16:27:13 +0000

    remove all the bypass code

--- perfmetrics/scripts/ml_tests/pytorch/dino/setup_container.sh
+++ perfmetrics/scripts/ml_tests/pytorch/dino/setup_container.sh
@@ -24,26 +24,6 @@ nohup /pytorch_dino/gcsfuse/gcsfuse --foreground --type-cache-ttl=1728000s \
         --log-format text \
        gke-gcs-fuse-csi-demo gcsfuse_data > "run_artifacts/gcsfuse.out" 2> "run_artifacts/gcsfuse.err" &
 
-# Update the pytorch library code to bypass the kernel-cache
-echo "Updating the pytorch library code to bypass the kernel-cache..."
-echo "
-def pil_loader(path: str) -> Image.Image:
-    fd = os.open(path, os.O_DIRECT)
-    f = os.fdopen(fd, \"rb\")
-    img = Image.open(f)
-    rgb_img = img.convert(\"RGB\")
-    f.close()
-    return rgb_img
-" > bypassed_code.py
-
-folder_file="/opt/conda/lib/python3.8/site-packages/torchvision/datasets/folder.py"
-x=$(grep -n "def pil_loader(path: str) -> Image.Image:" $folder_file | cut -f1 -d ':')
-y=$(grep -n "def accimage_loader(path: str) -> Any:" $folder_file | cut -f1 -d ':')
-y=$((y - 2))
-lines="$x,$y"
-sed -i "$lines"'d' $folder_file
-sed -i "$x"'r bypassed_code.py' $folder_file
-
 # Fix the caching issue - comes when we run the model first time with 8
 # nproc_per_node - by downloading the model in single thread environment.
 python -c 'import torch;torch.hub.list("facebookresearch/xcit:main")'
